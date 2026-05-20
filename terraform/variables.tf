60982085b292400c215dfaddfab60b8088ca6e2e
Matias Palacios <molinapalaciosjosuematias@gmail.com>
2026-05-20 19:47:51 -0400

    fix: corregir iam_instance_profile a LabInstanceProfile

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "app_port" {
 variable "iam_profile" {
   description = "Perfil IAM del Learner Lab"
   type        = string
-  default     = "voclabs" 
+  default     = "LabInstanceProfile"
 }
 
 variable "instance_type" {
